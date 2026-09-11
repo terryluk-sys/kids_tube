1d4980b40c30d8103a88252bf98bbefd0db56379
terryluk-sys <terryluk@wsmf.com.hk>
2026-09-11 10:05:15 +0800

    Add basic service worker implementation

---
+++ sw.js
@@ -0,0 +1,14 @@
+// Service Worker 基礎版本
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
